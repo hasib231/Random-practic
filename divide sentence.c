8a2f198a5a969cc5a7591343658181f94f01399c
Hasib <67379993+hasib231@users.noreply.github.com>
2021-05-21 01:26:43 +0600

    Add files via upload

---
+++ divide sentence.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[1000],word[100];
+    int i,j,length,is_word_started=0;
+    //gets(s);
+    //scanf("%[^\n]%*c",s);
+    fgets(s,sizeof(s),stdin);
+    length=strlen(s);
+    for(i=0,j=0;i<length;i++){
+        if(s[i]>='a' && s[i]<='z'){
+            if(is_word_started=0){
+                is_word_started=1;
+                word[j]='A'+s[i]-'a';
+                j++;
+            }
+            else{
+                word[j]=s[i];
+                j++;
+            }
+        }
+        else if(s[i]>='A'&&s[i]<='Z'){
+            if(is_word_started=0){
+                is_word_started=1;
+                }
+                word[j]=s[i];
+                j++;
+        }
+        else if(s[i]>='0'&&s[i]<='9'){
+            if(is_word_started=0){
+                is_word_started=1;
+                }
+                word[j]=s[i];
+                j++;
+        }
+        else{
+            if(is_word_started=1){
+                is_word_started=0;
+                word[j]='\0';
+                printf("%s\n",word);
+                j=0;
+                }
+        }
+
+    }
+    return 0;
+
+}
