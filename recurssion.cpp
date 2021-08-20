012a27fb4798e65fbd5f79cd3d51436435605dd2
Hasib <67379993+hasib231@users.noreply.github.com>
2021-08-20 21:33:08 +0600

    Update recurssion.cpp

--- recurssion.cpp
+++ recurssion.cpp
@@ -1,3 +1,29 @@
+//no 1
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int printNumber(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    return n+printNumber(n-1);
+
+}
+
+
+int main()
+{
+    int n,m,sum;
+    cin>>n;
+    cout<<endl;
+    sum=printNumber(n);
+    cout<<sum;
+}
+
+//no 2
 #include<bits/stdc++.h>
 
 using namespace std;
