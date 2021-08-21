eed17e1f82c44d9e1c86f4b75b9cd9ccc23b6a39
Hasib <67379993+hasib231@users.noreply.github.com>
2021-08-21 18:25:25 +0600

    Update recurssion.cpp

--- recurssion.cpp
+++ recurssion.cpp
@@ -211,4 +211,30 @@ int main()
     }
 }
 
+//fibonacci
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int fib(int n)
+{
+    if(n<=1)
+    {
+        return n;
+    }
+    return fib(n-1)+fib(n-2);
+
+}
+
+
+int main()
+{
+    int n,ans;
+    cin>>n;
+    cout<<endl;
+    ans=fib(n);
+    cout<<ans;
+}
+
+
 
